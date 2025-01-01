23f91b12b699a7e7b11726bd796c7e436fe55da9
Justin Kleiber <jkleiber8@gmail.com>
2025-01-01 12:23:36 -0500

    Scouting 2025 foundation (#70)
    * Re-architects project to use next.js and remove all the security vulnerabilities
    
    The site doesn't work yet, this commit just adds the next.js framework
    
    * Revert "Re-architects project to use next.js and remove all the security vulnerabilities"
    
    This reverts commit b263f5336461d1542498567f23ad93ed7f23b1ee.
    
    * Actually switches to vue since I'm familiar with it. Archives the old project to make it clear what is new and what is old
    
    * adds material icons properly
    
    * Table proof of concept begins
    
    * Creates sorted bar chart
    
    * Adds a dropdown for the graph view
    
    * Simple prototype of event view page
    
    * refactors to get a generic filterable graph
    
    * Begins working on the team page
    
    * Adds data tiles and examples to the team page
    
    * Attempts to get checks to pass

---
+++ archive/src/types.ts
@@ -0,0 +1,14 @@
+export type AverageData = {
+    teamNumber: number;
+    event: string;
+    matchesPlayed: number;
+    teleAmp: number;
+    teleSpeaker: number;
+    autoAmp: number;
+    autoSpeaker: number;
+    climb: number;
+    trapPercent: number;
+    taxiPercent: number;
+    autoAmpAccuracy: number;
+    teleAmpAccuracy: number;
+}
