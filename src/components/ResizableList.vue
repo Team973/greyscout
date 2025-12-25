2d145a8483f4492b516043f3868da5f381a59782
Justin Kleiber <jkleiber8@gmail.com>
2025-12-25 09:10:47 -0800

    Makes a resizable list for more generic view customization

---
+++ src/components/ResizableList.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+// TODO: fix types
+// @ts-nocheck
+
+import "@material/web/button/filled-button";
+
+</script>
+
+<template>
+    <div v-for="item, i in itemList">
+        <slot name="item-content" :index="i">
+        </slot>
+
+        <md-filled-button v-on:click="removeItem(i)" class="load-button">x</md-filled-button>
+    </div>
+    <div>
+        <md-filled-button v-on:click="addItem()" class="load-button">Add {{ itemTypeName }}</md-filled-button>
+    </div>
+</template>
+
+
+<script lang="ts">
+export default {
+    props: {
+        itemList: {
+            default: []
+        },
+        itemTypeName: {
+            default: "item"
+        }
+    },
+    emits: ["item-removed", "item-added"],
+    methods: {
+        removeItem(index: number) {
+            this.$emit("item-removed", index);
+        },
+        addItem() {
+            this.$emit("item-added", true)
+        },
+    }
+}
+</script>
+
+
+<style scoped></style>
